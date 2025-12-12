50f38ef934e55092a509aa48bbb1b41b92eaf12d
simone.dagostino <simone.dagostino@jp.kpmg.com>
2025-12-12 17:29:50 +0900

    update

--- src/view/Index.tsx
+++ src/view/Index.tsx
@@ -5,44 +5,42 @@ import * as modelIndex from "../model/Index";
 
 const viewIndex = (variableObject: modelIndex.Ivariable, methodObject: modelIndex.Imethod): IvirtualNode => {
     return (
-        <div class="full" jsmvcfw-controllerName="Index">
-            <div class="page_container view_index">
-                <div class="row">
-                    <a href="https://github.com/cimo" target="_blank">
-                        <img src="/asset/image/logo.png" class="logo cimo" />
-                    </a>
-                    <a href="https://www.typescriptlang.org/docs" target="_blank">
-                        <img src="/asset/image/typescript.svg" class="logo typescript" />
-                    </a>
-                    <a href="https://tauri.app" target="_blank">
-                        <img src="/asset/image/tauri.svg" class="logo tauri" />
-                    </a>
-                </div>
-                <div class="row">
-                    <button
-                        onclick={() => {
-                            methodObject.onClickCount();
-                        }}
-                    >
-                        Count
-                    </button>
-                    <p>
-                        Count: <span>{variableObject.count.state}</span>
-                    </p>
-                </div>
-                <div class="row">
-                    <input type="text" placeholder="Enter a name..." jsmvcfw-elementHookName="elementInputMessage" />
-                    <button
-                        onclick={() => {
-                            methodObject.onClickButton();
-                        }}
-                    >
-                        Send
-                    </button>
-                </div>
-                <div class="row">
-                    <p jsmvcfw-elementHookName="elementResultMessage"></p>
-                </div>
+        <div class="page_container view_index" jsmvcfw-controllerName="Index">
+            <div class="row">
+                <a href="https://github.com/cimo" target="_blank">
+                    <img src="/asset/image/logo.png" class="logo cimo" />
+                </a>
+                <a href="https://www.typescriptlang.org/docs" target="_blank">
+                    <img src="/asset/image/typescript.svg" class="logo typescript" />
+                </a>
+                <a href="https://tauri.app" target="_blank">
+                    <img src="/asset/image/tauri.svg" class="logo tauri" />
+                </a>
+            </div>
+            <div class="row">
+                <button
+                    onclick={() => {
+                        methodObject.onClickCount();
+                    }}
+                >
+                    Count
+                </button>
+                <p>
+                    Count: <span>{variableObject.count.state}</span>
+                </p>
+            </div>
+            <div class="row">
+                <input type="text" jsmvcfw-elementHookName="elementInputMessage" />
+                <button
+                    onclick={() => {
+                        methodObject.onClickButton();
+                    }}
+                >
+                    Send
+                </button>
+            </div>
+            <div class="row">
+                <p jsmvcfw-elementHookName="elementResultMessage"></p>
             </div>
         </div>
     );
